06023ed6b4c32942e01ccd3285b6d4c52d180a15
Allen Yen <nothing86617@gmail.com>
2024-08-02 15:16:15 +0800

    Initail commit

---
+++ src/components/course-component.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CourseService from "../services/course.service";
+
+const CourseComponent = ({
+  currentUser,
+  setCurrentUser,
+  patchCourseData,
+  setPatchCourseData,
+}) => {
+  const navigate = useNavigate();
+  const handleTakeToLogin = () => {
+    navigate("/login");
+  };
+  const [courseData, setCourseData] = useState(null);
+
+  const handleDelete = (_id) => {
+    console.log("執行handleDelete，id為" + _id);
+    CourseService.deleteCourseByID(_id)
+      .then(() => {
+        window.alert("課程已被刪除");
+        CourseService.get(currentUser.user._id)
+          .then((data) => {
+            setCourseData(data.data.foundCourses);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+
+  const handleDropOut = (_id) => {
+    console.log("執行handleDropOut，id為" + _id);
+    CourseService.dropOutCourseByID(_id)
+      .then(() => {
+        window.alert("課程已取消註冊");
+        CourseService.get(currentUser.user._id)
+          .then((data) => {
+            setCourseData(data.data.foundCourses);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+
+  useEffect(() => {
+    let _id;
+    if (currentUser) {
+      _id = currentUser.user._id;
+      if (currentUser.user.role == "instructor") {
+        CourseService.get(_id)
+          .then((data) => {
+            setCourseData(data.data.foundCourses);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else if (currentUser.user.role == "student") {
+        CourseService.getEnrolledCourse(_id)
+          .then((data) => {
+            setCourseData(data.data.foundCourses);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+    }
+  }, []);
+  return (
+    <div style={{ padding: "3rem " }}>
+      {!currentUser && (
+        <div>
+          <p>您必須先登入才能看到課程</p>
+          <button
+            onClick={handleTakeToLogin}
+            className="btn btn-primary btn-lg"
+          >
+            回到登入頁面
+          </button>
+        </div>
+      )}
+      {currentUser && currentUser.user.role == "instructor" && (
+        <div>
+          <h1>歡迎來到講師的課程頁面。</h1>
+        </div>
+      )}
+      {currentUser && currentUser.user.role == "student" && (
+        <div>
+          <h1>歡迎來到學生的課程頁面。</h1>
+        </div>
+      )}
+      {currentUser && courseData && courseData.length != 0 && (
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {courseData.map((course) => {
+            return (
+              <div className="card" style={{ width: "18rem", margin: "1rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">課程名稱:{course.title}</h5>
+                  <p style={{ margin: "0.5rem 0rem" }} className="card-text">
+                    {course.description}
+                  </p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    學生人數: {course.students.length}
+                  </p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    課程價格: {course.price}
+                  </p>
+                  {currentUser.user.role == "student" && (
+                    <button
+                      onClick={() => handleDropOut(course._id)}
+                      className="btn btn-danger"
+                      style={{ margin: "0.5rem 0.5rem" }}
+                    >
+                      <span>取消註冊</span>
+                    </button>
+                  )}
+                  {currentUser.user.role == "instructor" && (
+                    <div style={{ display: "flex", flexWrap: "wrap" }}>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => {
+                          setPatchCourseData(course);
+                          navigate("/patchCourse");
+                        }}
+                        style={{ margin: "0.5rem 0.5rem" }}
+                      >
+                        <span>修改課程</span>
+                      </button>
+                      <button
+                        onClick={() => handleDelete(course._id)}
+                        className="btn btn-danger"
+                        style={{ margin: "0.5rem 0.5rem" }}
+                      >
+                        <span>刪除課程</span>
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CourseComponent;
---
+++ src/components/patchCourse-component.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import CourseService from "../services/course.service";
+
+const PatchCourseComponent = (props) => {
+  let { currentUser, setCurrentUser, patchCourseData, setPatchCourseData } =
+    props;
+  let [title, setTitle] = useState(patchCourseData.title);
+  let [description, setDescription] = useState(patchCourseData.description);
+  let [price, setPrice] = useState(patchCourseData.price);
+  let [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleTakeToLogin = () => {
+    navigate("/login");
+  };
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleChangeDesciption = (e) => {
+    setDescription(e.target.value);
+  };
+  const handleChangePrice = (e) => {
+    setPrice(e.target.value);
+  };
+  const patchCourse = () => {
+    CourseService.patchCourseByID(
+      patchCourseData._id,
+      title,
+      description,
+      price
+    )
+      .then(() => {
+        window.alert("課程已更新成功");
+        navigate("/course");
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setMessage(error.response.data);
+      });
+  };
+
+  return (
+    <div style={{ padding: "3rem" }}>
+      {!currentUser && (
+        <div>
+          <p>在更新課程之前，您必須先登錄。</p>
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={handleTakeToLogin}
+          >
+            帶我進入登錄頁面。
+          </button>
+        </div>
+      )}
+      {currentUser && currentUser.user.role !== "instructor" && (
+        <div>
+          <p>只有講師才可以更新課程。</p>
+        </div>
+      )}
+      {currentUser && currentUser.user.role == "instructor" && (
+        <div className="form-group">
+          <div>
+            <h1>正在修改: {patchCourseData.title} 的課程資料</h1>
+          </div>
+          <label for="exampleforTitle">課程標題：</label>
+          <input
+            name="title"
+            type="text"
+            className="form-control"
+            id="exampleforTitle"
+            placeholder={patchCourseData.title}
+            onChange={handleChangeTitle}
+          />
+          <br />
+          <label for="exampleforContent">內容：</label>
+          <textarea
+            className="form-control"
+            id="exampleforContent"
+            aria-describedby="emailHelp"
+            name="content"
+            placeholder={patchCourseData.description}
+            onChange={handleChangeDesciption}
+          />
+          <br />
+          <label for="exampleforPrice">價格：</label>
+          <input
+            name="price"
+            type="number"
+            className="form-control"
+            id="exampleforPrice"
+            placeholder={patchCourseData.price}
+            onChange={handleChangePrice}
+          />
+          <br />
+          <button className="btn btn-primary" onClick={patchCourse}>
+            交出表單
+          </button>
+          <br />
+          <br />
+          {message && (
+            <div className="alert alert-warning" role="alert">
+              {message}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PatchCourseComponent;
